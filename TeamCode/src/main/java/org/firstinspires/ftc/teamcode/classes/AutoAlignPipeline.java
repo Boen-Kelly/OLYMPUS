31cdee8db52bfa2dd787ca849adb8cb69a02a911
bkelly <you@example.com>
2022-11-30 18:16:11 -0600

    working values, coded things to draw contour lines.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/classes/AutoAlignPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/classes/AutoAlignPipeline.java
@@ -8,6 +8,7 @@ import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
@@ -17,6 +18,9 @@ import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvPipeline;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Config
 public class AutoAlignPipeline {
     OpenCvCamera webcam;
@@ -25,8 +29,8 @@ public class AutoAlignPipeline {
     public static Rect midBox = new Rect(185,115,25,25);
     public static int threshVal = 128;
 
-    public static double LH = 90, LS = 170, LV = 230;
-    public static double UH = 100, US = 255, UV = 200;
+    public static double LH = 90, LS = 170, LV = 180;
+    public static double UH = 120, US = 255, UV = 255;
 
     public static int x = 10, y = 10;
 
@@ -43,7 +47,7 @@ public class AutoAlignPipeline {
             public void onOpened()
             {
                 webcam.setPipeline(thresh);
-                webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+                webcam.startStreaming(320, 240, OpenCvCameraRotation.SIDEWAYS_RIGHT);
             }
 
             @Override
@@ -79,13 +83,14 @@ public class AutoAlignPipeline {
         public void onViewportTapped() {
             stage ++;
 
-            if(stage > 2){
+            if(stage > 6){
                 stage = 0;
             }
         }
 
         @Override
         public Mat processFrame(Mat input){
+            List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
 
             Scalar LOWER_BOUND = new Scalar(LH,LS,LV);
             Scalar UPPER_BOUND = new Scalar(UH,US,UV);
@@ -103,12 +108,13 @@ public class AutoAlignPipeline {
 
             telemetry = "0: " + mask.get(x,y)[0] + "\nlength: " + mask.get(x,y).length + "\nlower & upper bound: " + LOWER_BOUND + "\n" + UPPER_BOUND;
 
-//            Imgproc.morphologyEx(mask, open, Imgproc.MORPH_OPEN, kernel);
-//            Imgproc.morphologyEx(mask, closed, Imgproc.MORPH_OPEN, kernel);
+            Imgproc.morphologyEx(mask, open, Imgproc.MORPH_OPEN, kernel);
+            Imgproc.morphologyEx(mask, closed, Imgproc.MORPH_OPEN, kernel);
 
-//            Core.add(open, closed, filtered);
+            Core.add(open, closed, filtered);
 
-//            Core.bitwise_and(input, input, output, mask);
+            Imgproc.findContours(filtered, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_NONE);
+            Imgproc.drawContours(input, contours, -1, new Scalar(0,255,0), 1);
 
             switch (stage){
                 case 0:
@@ -120,22 +126,22 @@ public class AutoAlignPipeline {
 //                    telemetry = "active stage is coi" + "\navg 1: " + avg1 + "\navg 2: " + avg2;
 //                    Imgproc.rectangle(greyscale, midBox,new Scalar(255,0,0), 2);
                     Imgproc.circle(mask, new Point(x,y), 2, new Scalar(255,0,0),-1);
-                    return mask;
+                    return hsv;
                 case 2:
                     telemetry = "active stage is threshold" + "\navg 1: " + avg1 + "\navg 2: " + avg2;
 //                    Imgproc.rectangle(threshold, midBox, new Scalar(255,0,0), 2);
                     Imgproc.circle(hsv, new Point(x,y), 2, new Scalar(255,0,0),-1);
-                    return hsv;
+                    return mask;
                 case 3:
                     telemetry = "active stage is input" + "\navg 1: " + avg1 + "\navg 2: " + avg2;
 //                    Imgproc.rectangle(input, midBox,new Scalar(255,0,0), 2);
-                    return h;
+                    return open;
                 case 4:
-                    return s;
+                    return closed;
                 case 5:
-                    return v;
+                    return filtered;
                 case 6:
-                    return output;
+                    return input;
                 default:
                     return output;
             }
