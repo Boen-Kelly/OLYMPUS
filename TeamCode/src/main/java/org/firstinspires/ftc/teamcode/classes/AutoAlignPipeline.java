cabd3578c5b6992b0ae532a974dae9422e5e6cbd
bkelly <you@example.com>
2022-11-30 22:13:57 -0600

    detection is working! Might have to tune some values, but I can definitely detect poles, and determine whether I am to the left or right of said pole.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/classes/AutoAlignPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/classes/AutoAlignPipeline.java
@@ -34,6 +34,9 @@ public class AutoAlignPipeline {
     public static double UH = 120, US = 255, UV = 255;
 
     public static int x = 10, y = 10;
+    public static double boxWidth = 10;
+
+    polePos pos = polePos.ON_POINT;
 
 
     public AutoAlignPipeline(HardwareMap hardwareMap, String camName){
@@ -78,6 +81,9 @@ public class AutoAlignPipeline {
 
         Mat kernel = new Mat(12,12, CvType.CV_8UC1);
 
+        double maxWidth = 0;
+        double distance = 0;
+
 
         double avg1, avg2;
 
@@ -108,30 +114,53 @@ public class AutoAlignPipeline {
 
             Core.inRange(hsv, LOWER_BOUND, UPPER_BOUND, mask);
 
-            telemetry = "0: " + mask.get(x,y)[0] + "\nlength: " + mask.get(x,y).length + "\nlower & upper bound: " + LOWER_BOUND + "\n" + UPPER_BOUND;
-
             Imgproc.morphologyEx(mask, open, Imgproc.MORPH_OPEN, kernel);
             Imgproc.morphologyEx(mask, closed, Imgproc.MORPH_OPEN, kernel);
 
             Core.add(open, closed, filtered);
 
             Imgproc.findContours(filtered, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_NONE);
-            Imgproc.drawContours(input, contours, -1, new Scalar(0,255,0), 1);
+//            Imgproc.drawContours(input, contours, -1, new Scalar(0,255,0), 1);
 
-            boundingRect = Imgproc.boundingRect(filtered);
 
-            Imgproc.rectangle(input,boundingRect, new Scalar(255,0,0), 2);
+            for(Mat box:contours) {
+                boundingRect = Imgproc.boundingRect(box);
 
-            top = new Point(boundingRect.x + boundingRect.width*.5,boundingRect.y);
-            bottom = new Point(boundingRect.x + boundingRect.width*.5, boundingRect.y +boundingRect.height);
+                if(boundingRect.width > maxWidth){
+                    maxWidth = boundingRect.width;
 
-            Imgproc.line(input, top,bottom, new Scalar(255,0,0));
+                    Imgproc.rectangle(input, boundingRect, new Scalar(255, 0, 0), 2);
+
+                    top = new Point(boundingRect.x + boundingRect.width * .5, boundingRect.y);
+                    bottom = new Point(boundingRect.x + boundingRect.width * .5, boundingRect.y + boundingRect.height);
+
+                    Imgproc.line(input, top, bottom, new Scalar(255, 0, 0));
+                }
+            }
+
+            if(top != null) {
+                distance = top.x - 120;
+                Imgproc.line(input, new Point(top.x, 10), new Point(120, 10), new Scalar(0, 255, 0), 2);
+            }
+
+            Imgproc.rectangle(input,new Point(120-boxWidth/2,5), new Point(120+boxWidth/2,15), new Scalar(0,0,255), 2);
+
+            if(distance > boxWidth/2){
+                pos = polePos.RIGHT;
+            }else if(distance < -boxWidth/2){
+                pos = polePos.LEFT;
+            }else{
+                pos = polePos.ON_POINT;
+            }
+
+            telemetry = "contours.length: " + contours.size() + "\nwidth: " + maxWidth + "\ndistance: " + distance + "\npos: " + pos;
+
+            maxWidth = 0;
 
             switch (stage){
                 case 0:
 //                    telemetry = "active stage is ycrcb" + "\navg 1: " + avg1 + "\navg 2: " + avg2;
 //                    Imgproc.rectangle(greyscale, midBox,new Scalar(255,0,0), 2);
-                    Imgproc.circle(input, new Point(x,y), 2, new Scalar(255,0,0),-1);
                     return input;
                 case 1:
 //                    telemetry = "active stage is coi" + "\navg 1: " + avg1 + "\navg 2: " + avg2;
@@ -161,5 +190,11 @@ public class AutoAlignPipeline {
 
     public String toString(){
         return telemetry;
+    }
+
+    public enum polePos{
+        LEFT,
+        RIGHT,
+        ON_POINT
     }
 }
